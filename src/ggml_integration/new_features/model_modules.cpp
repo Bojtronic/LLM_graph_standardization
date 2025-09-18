ec249554be4c296fbd0072315d08da72561b3713
Bojtronic <bojtronic@gmail.com>
2025-09-18 10:46:01 -0600

    Debug for ggml rope parameters

--- src/ggml_integration/new_features/model_modules.cpp
+++ src/ggml_integration/new_features/model_modules.cpp
@@ -255,7 +255,8 @@ ggml_tensor * positional_encoding(ggml_context * ctx, ggml_tensor * input, const
 
         printf("DEBUG - Input tensor dimensions: [%ld, %ld, %ld, %ld]\n", 
            input->ne[0], input->ne[1], input->ne[2], input->ne[3]);
-        printf("DEBUG - Positions tensor dimensions: [%ld]\n", positions->ne[0]);
+        printf("DEBUG - Positions tensor dimensions: [%ld, %ld, %ld, %ld]\n", 
+            positions->ne[0], positions->ne[1], positions->ne[2], positions->ne[3]);
         printf("DEBUG - Assertion requires: input->ne[2] (%ld) == positions->ne[0] (%ld)\n", 
             input->ne[2], positions->ne[0]);
 
