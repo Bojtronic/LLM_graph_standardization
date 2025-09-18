14ff3dbb128035b81bbbb40415baef778a2849d4
Bojtronic <bojtronic@gmail.com>
2025-09-18 12:06:57 -0600

    Debug for ggml rope parameters

--- src/ggml_integration/new_features/model_modules.cpp
+++ src/ggml_integration/new_features/model_modules.cpp
@@ -246,7 +246,7 @@ ggml_tensor * positional_encoding(ggml_context * ctx, ggml_tensor * input, const
     if (strcmp(type, "rope") == 0) {
 
         // Permute to [n_head, head_size, seq_len, 1]
-        input = ggml_permute(ctx, input, 1, 2, 0, 3);
+        //input = ggml_permute(ctx, input, 1, 2, 0, 3);
 
         // Crea un tensor para almacenar las posiciones (índices de secuencia) como I32
         ggml_tensor * positions = ggml_new_tensor_1d(ctx, GGML_TYPE_I32, input->ne[2]);
